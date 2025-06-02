a00f4d240bb66724563cb86566cc920c18054011
RushoBinnabi <69098829+RushoBinnabi@users.noreply.github.com>
2025-06-02 10:03:32 -0400

    Added more functionality.

--- PhotoReaderGUI.py
+++ PhotoReaderGUI.py
@@ -19,19 +19,19 @@ browseFilesButton = tkinter.Button(mainWindow, text="Browse Files", command=Phot
 browseFilesButton.pack(side=tkinter.TOP, padx=10, pady=50)
 browseFilesButton.place(x=300, y=35)
 
-analyzeImageText = tkinter.Button(mainWindow, text="Analyze Image Text")
+analyzeImageText = tkinter.Button(mainWindow, text="Analyze Image Text", command=PhotoReaderLauncherAndCommands.analyzeImageTextCommand)
 analyzeImageText.pack(side=tkinter.TOP, padx=10, pady=50)
 analyzeImageText.place(x=120, y=78)
 
-clearButton = tkinter.Button(mainWindow, text="Clear Image and Text")
+clearButton = tkinter.Button(mainWindow, text="Clear Image and Text", command=PhotoReaderLauncherAndCommands.clearImageAndTextCommand)
 clearButton.pack(side=tkinter.TOP, padx=10, pady=50)
 clearButton.place(x=250, y=78)
 
-leftArrowButton = tkinter.Button(mainWindow, image=leftArrowIconImage)
+leftArrowButton = tkinter.Button(mainWindow, image=leftArrowIconImage, command=PhotoReaderLauncherAndCommands.leftArrowCommand)
 leftArrowButton.pack(side=tkinter.TOP, padx=10, pady=50)
 leftArrowButton.place(x=185, y=120, width=40)
 
-rightArrowButton = tkinter.Button(mainWindow, image=rightArrowIconImage)
+rightArrowButton = tkinter.Button(mainWindow, image=rightArrowIconImage, command=PhotoReaderLauncherAndCommands.rightArrowCommand)
 rightArrowButton.pack(side=tkinter.TOP, padx=10, pady=50)
 rightArrowButton.place(x=250, y=120, width=40)
 
--- PhotoReaderLauncherAndCommands.py
+++ PhotoReaderLauncherAndCommands.py
@@ -1,9 +1,40 @@
 import tkinter as tk
+from tkinter import PhotoImage
 from tkinter.filedialog import askopenfilename
+
+from PIL import Image, ImageTk
+
 import PhotoReaderGUI
+import pytesseract
+
+image = ""
+imageText = ""
 
 def browseFilesCommand():
     fn = askopenfilename()
     PhotoReaderGUI.filepathEntryWidget.config(state=tk.NORMAL)
     PhotoReaderGUI.filepathEntryWidget.insert(tk.END, fn)
     PhotoReaderGUI.filepathEntryWidget.config(state=tk.DISABLED)
+
+def clearImageAndTextCommand():
+    PhotoReaderGUI.filepathEntryWidget.config(state=tk.NORMAL)
+    PhotoReaderGUI.filepathEntryWidget.delete(0, tk.END)
+    PhotoReaderGUI.filepathEntryWidget.config(state=tk.DISABLED)
+    PhotoReaderGUI.outputArea.config(state=tk.NORMAL)
+    PhotoReaderGUI.outputArea.delete(1.0, tk.END)
+    PhotoReaderGUI.outputArea.config(state=tk.DISABLED)
+
+def analyzeImageTextCommand():
+    PhotoReaderGUI.filepathEntryWidget.config(state=tk.NORMAL)
+    PhotoReaderGUI.outputArea.config(state=tk.NORMAL)
+    inserted_image = Image.open(PhotoReaderGUI.filepathEntryWidget.get())
+    imageText = pytesseract.image_to_string(inserted_image)
+    # finish functionality here.
+    PhotoReaderGUI.filepathEntryWidget.config(state=tk.DISABLED)
+    PhotoReaderGUI.outputArea.config(state=tk.DISABLED)
+
+def leftArrowCommand():
+    print()
+
+def rightArrowCommand():
+    print()
